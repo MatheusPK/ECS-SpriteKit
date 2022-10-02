d144500119b4f961d73d8d065462e45a5000d8e0
MatheusPK <matheuskulick@hotmail.com>
2022-10-01 23:00:14 -0300

    [DEL] Main storyboard

--- GameEngine/GameEngine/GameViewController.swift
+++ GameEngine/GameEngine/GameViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GameViewController.swift
 //  GameEngine
 //
 //  Created by Matheus P.K on 01/10/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
