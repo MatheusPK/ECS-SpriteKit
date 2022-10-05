a0054cbf262099b0d01eb0d567ff34c7386f22f1
MatheusPK <matheuskulick@hotmail.com>
2022-10-05 00:34:18 -0300

    [ADD] Custom move set facetrackinginput

--- GameEngine/GameEngine/ECS/Components/FaceTrackingMovementComponent.swift
+++ GameEngine/GameEngine/ECS/Components/FaceTrackingMovementComponent.swift
@@ -12,9 +12,14 @@ import ARKit
 class FaceTrackingMovementComponent: GKComponent {
     
     let faceTrackingView: FaceTrackingView
+    let faceExpressionSet: FaceExpressionSet
     
-    override init() {
-        faceTrackingView = FaceTrackingView()
+    private var moveSet: FaceExpressionsToDirection
+    
+    init(faceExpressionSet: FaceExpressionSet = .original) {
+        self.faceTrackingView = FaceTrackingView()
+        self.faceExpressionSet = faceExpressionSet
+        self.moveSet = faceExpressionSet.getMovementSet()
         super.init()
     }
     
@@ -31,29 +36,16 @@ class FaceTrackingMovementComponent: GKComponent {
 extension FaceTrackingMovementComponent: ARSessionDelegate {
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         guard let faceAnchor = anchors.first as? ARFaceAnchor else { return }
-        
-        guard let browInnerUp = faceAnchor.blendShapes[.browInnerUp] as? Float else { return }
-        guard let leftBrownDown = faceAnchor.blendShapes[.browDownLeft] as? Float else { return }
-        guard let rightBrownDown = faceAnchor.blendShapes[.browDownLeft] as? Float else { return }
-        guard let mouthLeft = faceAnchor.blendShapes[.mouthRight] as? Float else { return }
-        guard let mouthRight = faceAnchor.blendShapes[.mouthLeft] as? Float else { return }
-        
+        handleFaceExpression(faceAnchor: faceAnchor)
+    }
+    
+    private func handleFaceExpression(faceAnchor: ARFaceAnchor) {
         var directions: [MovementComponent.Direction] = [.idle]
         
-        if browInnerUp > 0.5 {
-            directions.append(.up)
-        }
-        
-        if (leftBrownDown > 0.3 && rightBrownDown > 0.3) {
-            directions.append(.down)
-        }
-        
-        if mouthLeft > 0.4 {
-            directions.append(.left)
-        }
-        
-        if mouthRight > 0.4 {
-            directions.append(.right)
+        for (direction, faceExpression) in moveSet {
+            if faceExpression.reachedFactor(faceAnchor: faceAnchor) {
+                directions.append(direction)
+            }
         }
         
         if let movementComponent = entity?.component(ofType: MovementComponent.self) {
@@ -62,6 +54,72 @@ extension FaceTrackingMovementComponent: ARSessionDelegate {
             }
         }
     }
+}
+
+extension FaceTrackingMovementComponent {
+    
+    typealias FaceExpressionsToDirection = [MovementComponent.Direction : FaceExpression]
+    
+    enum FaceExpressionSet {
+        case original
+        case alternative
+        case custom(up: FaceExpression,
+                    down: FaceExpression,
+                    left: FaceExpression,
+                    right: FaceExpression)
+        
+        fileprivate func getMovementSet() -> FaceExpressionsToDirection {
+            var movementSet: FaceExpressionsToDirection
+            switch self {
+                case .original:
+                    movementSet = [
+                        .up : FaceExpression(expressions: [.browInnerUp], factor: 0.5),
+                        .down : FaceExpression(expressions: [.browDownLeft, .browDownRight], factor: 0.3),
+                        .left : FaceExpression(expressions: [.mouthRight], factor: 0.4),
+                        .right : FaceExpression(expressions: [.mouthLeft], factor: 0.4)
+                    ]
+                case .alternative:
+                    movementSet = [
+                        .up : FaceExpression(expressions: [.mouthPucker], factor: 0.4),
+                        .down : FaceExpression(expressions: [.tongueOut], factor: 0.4),
+                        .left : FaceExpression(expressions: [.eyeBlinkRight], factor: 0.7),
+                        .right : FaceExpression(expressions: [.eyeBlinkLeft], factor: 0.7)
+                    ]
+                case .custom(up: let up, down: let down, left: let left, right: let right):
+                    movementSet = [
+                        .up : up,
+                        .down : down,
+                        .left : left,
+                        .right : right
+                    ]
+            }
+            return movementSet
+        }
+        
+    }
+
+}
+
+struct FaceExpression {
+    
+    let expressions: [ARFaceAnchor.BlendShapeLocation]
+    let factor : Float
+    
+    func reachedFactor(faceAnchor: ARFaceAnchor) -> Bool {
+        for expression in expressions {
+            if getFactor(for: expression, in: faceAnchor) < factor {
+                return false
+            }
+        }
+        return true
+    }
+    
+    private func getFactor(for expression: ARFaceAnchor.BlendShapeLocation?, in faceAnchor: ARFaceAnchor) -> Float {
+        guard let expression = expression else { return .zero }
+        let factor = faceAnchor.blendShapes[expression] as? Float
+        return factor ?? .zero
+    }
+    
 }
 
 
